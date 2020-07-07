02b87f7df59b43c712c2fab7a4fa37e5ffb9df73
Shaimay <shaimay@Shaimays-MacBook-Air.local>
2020-07-07 23:39:14 +0530

    Updates

---
+++ precache-manifest.08e8f3a3406a5afe3d500b5d785ee1e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96262bd8a91ae9e8f298018586e92b7a",
+    "url": "/FIT-2/index.html"
+  },
+  {
+    "revision": "a44b21b9ee24c0978817",
+    "url": "/FIT-2/static/css/main.a8202b88.chunk.css"
+  },
+  {
+    "revision": "3c84b29c7d2ef0ea44a2",
+    "url": "/FIT-2/static/js/2.a648631e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/FIT-2/static/js/2.a648631e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44b21b9ee24c0978817",
+    "url": "/FIT-2/static/js/main.d5e69689.chunk.js"
+  },
+  {
+    "revision": "461d315db25ee372d1e9",
+    "url": "/FIT-2/static/js/runtime-main.0440aedc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/FIT-2/static/media/image.d7265326.png"
+  }
+]);
